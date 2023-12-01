a5279d4f52ca1cd0fc406d0bfb1d81f4d52ebdb0
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-12-01 09:24:23 -0500

    sasaasassa

--- main.js
+++ main.js
@@ -16,17 +16,22 @@ Aaron.then((result => { //do NOT use .then
 */
 
 //REST API 
-const URL = `https://api.quotable.io/random`; //${pokemon}
+const URL = `https://api.quotable.io/randoms`; //${pokemon}
 
 async function getData(URL){
     try {
         const response = await fetch(URL); //await gets promise (?)
+        if(response.status !=200){ //>299
+            throw new Error(response.statusText);
+        }
         console.log(response);
         const data = await response.json();
         console.log(data);
         document.querySelector("h1").textContent = data.content; //shift alt down
-        document.querySelector("h2").textContent = data.author;
-    } catch (error) {}
+        document.querySelector("h2").textContent = data.content;
+    } catch (error) {
+        document.querySelector("h1").textContent = error;
+    }
 }
 getData(URL);
 
