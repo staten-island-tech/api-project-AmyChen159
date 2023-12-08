3bd7f2b4fbec28153beaf5c1655fb4068213cee1
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-12-08 09:24:33 -0500

    ewfafsaas

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
 import '../styles/style.css'
 
-const URL = `https://api.genshin.dev/`; 
+const URL = `https://api.genshin.dev/artifacts`; 
 
 async function getData(URL){
     try {
@@ -15,7 +15,7 @@ async function getData(URL){
         //document.querySelector("h2").textContent = data.author;
         /* document.querySelector('#container').insertAdjacentHTML(
           `beforeend`,
-          `<h2>${data.artifacts.name}</h2>`
+          `<h2>${data.artifacts}</h2>`
         ) */
     } catch (error) {
         document.querySelector("h1").textContent = error;
@@ -23,21 +23,7 @@ async function getData(URL){
 }
 getData(URL);
 
-function createCard(cards) {
-  cards.forEach(card => {
-    document.querySelector('#container').insertAdjacentHTML(
-      `beforeend`,
-      `<div class="gallery">
-        <h2 id="charactername">${card.name}</h2> 
-        <img id="imageicon" src="${card.icon}"/>
-        <h3 class="bottomtext">${card.element} | ${card.weapon}</h3>
-        <h3 class="bottomtext">${card.region}</h3>
-        <h3 class="bottomtext">${card.rarity}</h3>
-      </div>`) 
-  })
-}
 
-createCard(cards)
 
 
 
